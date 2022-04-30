40583eea5d9fed4a38f90038d8482f6422978b3a
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-04-30 02:27:09 +0000

    Changed default layout to wide

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import streamlit as st
 import plotly.express as px
 import os
 from src.spectrometry import plot_spectrometry, WAVELENGTH_IDENTIFIERS
+from src.containers.predict_soc import predict_soc_container
 
 mapbox_token = os.environ["MAPBOX_TOKEN"]
 px.set_mapbox_access_token(mapbox_token)
 
+st.set_page_config(layout="wide")
 st.title("spectral hub (demo)")
 
 
@@ -29,33 +31,4 @@ if __name__ == "__main__":
     with st.container():
         st.plotly_chart(soc_map())
 
-    with st.container():
-        st.header("Predict SOC")
-        st.caption("Upload your own spectrometry data to predic soc")
-        spec_data = st.file_uploader("Upload")
-
-        if spec_data != None:
-            uploaded_df = pd.read_csv(spec_data)
-            st.write(uploaded_df)
-
-            id_col_name: str = st.selectbox(
-                "My Sample ID column is", uploaded_df.columns.values.tolist()
-            )
-            wavelength_identifier = st.selectbox(
-                "My measurements are", WAVELENGTH_IDENTIFIERS.keys()
-            )
-
-            st.button("Predict SOC")
-
-            st.header("Explore individual samples")
-            selected_sample_id = st.selectbox(
-                "Select a sample", uploaded_df[uploaded_df.columns.values[0]].to_list()
-            )
-
-            st.plotly_chart(
-                plot_spectrometry(
-                    uploaded_df[uploaded_df[id_col_name] == selected_sample_id],
-                    id_col_name,
-                    WAVELENGTH_IDENTIFIERS[wavelength_identifier],
-                )
-            )
+    predict_soc_container()
