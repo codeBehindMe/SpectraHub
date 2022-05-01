e47d4f535e92fc2a9aafe047293ccd10a24ff55d
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-05-01 06:56:13 +0000

    Added percentage identifier to error

--- src/models/soc.py
+++ src/models/soc.py
@@ -7,6 +7,6 @@ def spectrometry_based_soc_model(df: pd.DataFrame, key_col: str):
     subset_key = pd.DataFrame(df[key_col])
 
     subset_key["PREDICTED_SOC_PERC"] = np.random.uniform(1, 100, df.shape[0])
-    subset_key["PREDICTION_ERROR"] = np.random.uniform(5, 15, df.shape[0])
+    subset_key["PREDICTION_ERROR_PERC"] = np.random.uniform(5, 15, df.shape[0])
 
     return subset_key
